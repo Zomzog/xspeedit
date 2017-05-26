6c1396b842f4ba3aa4e2bd6767492769714ab752
Zomzog <zomzog@gmail.com>
2017-05-26 22:46:58 +0200

    Bigger first

--- src/main/java/bzh/zomzog/vsct/xspeedIt/PackageSorter.java
+++ src/main/java/bzh/zomzog/vsct/xspeedIt/PackageSorter.java
@@ -1,24 +1,57 @@
 package bzh.zomzog.vsct.xspeedIt;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class PackageSorter {
 
     public static final String SEPARATOR = "/";
 
+    public static final int MAX_ARTICLE_SIZE = 9;
+    public static final int MAX_PACKAGE_SIZE = 10;
+
+    /**
+     * 
+     * @param input
+     * @return
+     */
     public static String sort(final String input) {
-        final StringBuilder sb = new StringBuilder();
-        int currentPackageContent = 0;
-        for (int i = 0; i < input.length(); i++) {
-            final char c = input.charAt(i);
-            final int size = Character.getNumericValue(c);
-            if (currentPackageContent + size > 10) {
-                sb.append(SEPARATOR);
-                currentPackageContent = size;
-            } else {
-                currentPackageContent += size;
+        // Group all package by size
+        final Map<Integer, Long> packages = input.chars()
+                .mapToObj(i -> (char) i)
+                .map(c -> Character.getNumericValue(c))
+                .collect(
+                        Collectors.groupingBy(
+                                Function.identity(), Collectors.counting()));
+
+        final List<String> result = new ArrayList<>();
+
+        for (int i = MAX_ARTICLE_SIZE; i > 0; i--) {
+            Long currentSizeAvailable = packages.get(i);
+            while (currentSizeAvailable != null
+                    && currentSizeAvailable > 0) {
+                currentSizeAvailable--;
+                packages.put(i, currentSizeAvailable);
+                final StringBuilder currentPackageContent = new StringBuilder().append(i);
+
+                int currentPackageContentSize = i;
+                for (int j = i; j > 0; j--) {
+                    final Long newSizeAvailable = packages.get(j);
+                    if (newSizeAvailable != null
+                            && newSizeAvailable > 0
+                            && currentPackageContentSize + j <= MAX_PACKAGE_SIZE) {
+                        currentPackageContentSize += newSizeAvailable;
+                        packages.put(j, newSizeAvailable - 1);
+                        currentPackageContent.append(j);
+                    }
+                }
+                result.add(currentPackageContent.toString());
             }
-            sb.append(c);
         }
-        return sb.toString();
+        return result.stream().collect(Collectors.joining("/"));
     }
 
 }
--- src/test/java/bzh/zomzog/vsct/xspeedIt/PackageSorterTest.java
+++ src/test/java/bzh/zomzog/vsct/xspeedIt/PackageSorterTest.java
@@ -14,9 +14,23 @@ public class PackageSorterTest {
         final String input = "191919";
 
         final String result = PackageSorter.sort(input);
-
+        System.out.println("Input  : " + input);
+        System.out.println("Result : " + result);
         final String[] packageList = result.split(PackageSorter.SEPARATOR);
         assertThat(packageList).hasSize(3);
     }
 
+    /**
+     * Best sort : "19/19/19" => 3 box
+     */
+    @Test
+    public void complexeSort() {
+        final String input = "111999";
+
+        final String result = PackageSorter.sort(input);
+        System.out.println("Input  : " + input);
+        System.out.println("Result : " + result);
+        final String[] packageList = result.split(PackageSorter.SEPARATOR);
+        assertThat(packageList).hasSize(3);
+    }
 }
